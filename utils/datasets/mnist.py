de96d482896ba8576bb0f6ce34ab5440ad487ceb
Mohammed Fellaji <fellajimohammed@gmail.com>
2022-07-19 13:03:38 +0200

    mnist getter

--- utils/datasets/mnist.py
+++ utils/datasets/mnist.py
@@ -1,17 +1,18 @@
 import torch
 import torch.distributions
 from torchvision import datasets, transforms
-from torchvision.datasets.vision import VisionDataset
 
+from .paths import get_mnist_path
 DEFAULT_TRAIN_BATCHSIZE = 128
 DEFAULT_TEST_BATCHSIZE = 512
 
+
 def MNIST(train=True, batch_size=None, augm_flag=True, shuffle=None):
-    if batch_size==None:
+    if batch_size is None:
         if train:
-            batch_size=DEFAULT_TRAIN_BATCHSIZE
+            batch_size = DEFAULT_TRAIN_BATCHSIZE
         else:
-            batch_size=DEFAULT_TEST_BATCHSIZE
+            batch_size = DEFAULT_TEST_BATCHSIZE
 
     if shuffle is None:
         shuffle = train
@@ -22,14 +23,17 @@ def MNIST(train=True, batch_size=None, augm_flag=True, shuffle=None):
     ] + transform_base)
     transform_test = transforms.Compose(transform_base)
 
-    transform_train = transforms.RandomChoice([transform_train, transform_test])
+    transform_train = transforms.RandomChoice([transform_train,
+                                               transform_test])
 
     transform = transform_train if (augm_flag and train) else transform_test
 
+    path = get_mnist_path()
     dataset = datasets.MNIST(path, train=train, transform=transform)
     loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                          shuffle=shuffle, num_workers=4)
     return loader
+
 
 def EMNIST(train=False, batch_size=None, augm_flag=False, shuffle=None):
     if batch_size==None:
